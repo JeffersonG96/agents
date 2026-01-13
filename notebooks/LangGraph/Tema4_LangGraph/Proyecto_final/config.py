26bde0c67287b174d8b39419caeb9832f4552f14
JeffersonG96 <jefersonguaman94@gmail.com>
2026-01-12 21:53:59 -0500

    proyecto con Rag y Langgraph

---
+++ notebooks/LangGraph/Tema4_LangGraph/Proyecto_final/config.py
@@ -0,0 +1,3 @@
+CHROMADB_PATH="D:\\Cursos\\LLM\\proyectos\\agentes\\introduccion\\notebooks\\LangGraph\\Tema4_LangGraph\\Proyecto_final\\chroma_db"
+DOCS_PATH="D:\\Cursos\\LLM\\proyectos\\agentes\\introduccion\\notebooks\\LangGraph\\Tema4_LangGraph\\Proyecto_final\\docs"
+EMBEDDINGS_MODEL = "text-embedding-3-large"
